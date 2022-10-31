4163bf7ba25fc85e77e3d95ff2ca7754058404a8
Darric Heng <darricheng@gmail.com>
2022-10-31 22:56:38 +0800

    Set up proper app routing structure

--- src/Containers/AppContainer.js
+++ src/Containers/AppContainer.js
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../Components/Home";
+import NotesLibrary from "../Components/NotesLibrary";
 import Search from "../Components/Search";
+import BooksSidebar from "../Layouts/BooksSidebar";
 
 export default function AppContainer() {
   return (
     <div className="app-container">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route element={<BooksSidebar />}>
+          <Route path="/search" element={<Search />} />
+          <Route path="/notes-library" element={<NotesLibrary />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/Layouts/BooksSidebar.js
+++ src/Layouts/BooksSidebar.js
@@ -1,3 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 export default function BooksSidebar() {
-  return;
+  return (
+    <div>
+      <h1>some sidebar stuff</h1>
+      <div className="another-container">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
