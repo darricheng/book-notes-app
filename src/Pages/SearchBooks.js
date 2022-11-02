52c776c16de4b3ed3850cb64a9f16b395353f6ec
Darric Heng <darricheng@gmail.com>
2022-11-02 20:30:37 +0800

    Add cards to display search results of books

--- src/Pages/SearchBooks.js
+++ src/Pages/SearchBooks.js
@@ -1,9 +1,18 @@
+import BookCard from "../Components/BookCard";
 import SearchBar from "../Components/SearchBar";
 
 export default function SearchBooks() {
   return (
     <>
       <SearchBar />
+      <div className="search-results-container mt-12 flex flex-row flex-wrap justify-between gap-y-12">
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+      </div>
     </>
   );
 }
