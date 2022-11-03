236826e795e5d82c67f7b8625f229017d758ee51
Darric Heng <darricheng@gmail.com>
2022-11-03 19:20:25 +0800

    Sidebar updates

--- src/Components/SidebarCard.js
+++ src/Components/SidebarCard.js
@@ -3,7 +3,7 @@ export default function SidebarCard({ title }) {
     <li>
       <div
         href="#"
-        className="flex items-center p-2 rounded-lg text-base font-normal text-gray-900 dark:text-white bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+        className="flex items-center p-2 rounded-lg text-base font-normal text-gray-900 dark:text-white bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 hover:cursor-pointer"
       >
         <span className="ml-3">{title}</span>
       </div>
