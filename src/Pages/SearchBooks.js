aa0552645bbb5d84b8360c7bbb85fcc824b84e7a
Darric Heng <darricheng@gmail.com>
2022-11-03 11:04:52 +0800

    Able to update redux state

--- src/Pages/SearchBooks.js
+++ src/Pages/SearchBooks.js
@@ -4,6 +4,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { addToCollection } from "../Redux/librarySlice";
 import { useEffect } from "react";
 
+class LibraryBook {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.notes = [];
+  }
+}
+
 export default function SearchBooks({
   // destructured props
   setSearchText,
@@ -13,7 +22,21 @@ export default function SearchBooks({
   const lib = useSelector((state) => state.library.value);
   const dispatch = useDispatch();
 
-  const cardClick = (data) => dispatch(addToCollection(data));
+  const cardClick = (data) => {
+    /* Use destructuring to convert class Object to a plain JS object
+		Source: https://stackoverflow.com/questions/34699529/convert-javascript-class-instance-to-plain-object-preserving-methods
+		
+		# Why?
+    - Payload passed to redux needs to be serializable: https://redux.js.org/faq/actions#why-should-type-be-a-string-or-at-least-serializable-why-should-my-action-types-be-constants
+		- Only plain objects are serializable: https://developer.mozilla.org/en-US/docs/Glossary/Serializable_object#supported_objects
+		*/
+    const { ...book } = new LibraryBook(
+      data.title,
+      data.author_name,
+      data.isbn
+    );
+    dispatch(addToCollection(book));
+  };
 
   useEffect(() => {
     console.log(lib);
