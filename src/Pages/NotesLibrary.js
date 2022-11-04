f6c6d4aa38e2a6edd0bb46a4ec8eab6fa44f98da
Darric Heng <darricheng@gmail.com>
2022-11-04 21:01:45 +0800

    Library notes visual updates

--- src/Pages/NotesLibrary.js
+++ src/Pages/NotesLibrary.js
@@ -7,9 +7,12 @@ export default function NotesLibrary() {
 
   return (
     <div className="notes-lib relative">
-      <h1>Select a book from the sidebar to begin</h1>
-      <button className="sticky top-32 left-9/10 -translate-y-8 z-50 px-4 py-1 bg-blue-400 rounded-full text-white">
-        Save
+      <h1 className="text-4xl font-bold">
+        Harry Potter and the Order of the Phoenix
+      </h1>
+      <h2 className="text-lg font-semibold">J.K. Rowling</h2>
+      <button className="sticky top-32 left-9/10 -translate-y-8 z-50 px-6 py-1 bg-blue-500 hover:bg-blue-400 rounded-full text-white text-xl font-semibold">
+        Save Notes
       </button>
       <Note />
       <Note />
