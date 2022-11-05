4f12cfb7a24428e0e6d987a0249ac6cf3c60d102
Darric Heng <darricheng@gmail.com>
2022-11-05 20:37:33 +0800

    Change SidebarCard from useNavigate to NavLink

--- src/Components/SidebarCard.js
+++ src/Components/SidebarCard.js
@@ -1,20 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function SidebarCard({ title, index, isbn }) {
-  const navigate = useNavigate();
-
   return (
     <li>
-      {/* <NavLink to={`/notes-library/${isbn}`}> */}
-      <div
-        onClick={() => navigate(`/notes-library/${isbn}`)}
-        key={index}
-        isbn={isbn}
-        className="flex items-center p-2 rounded-lg text-base font-normal text-gray-900 dark:text-white bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 hover:cursor-pointer"
-      >
-        <span className="ml-3">{title}</span>
-      </div>
-      {/* </NavLink> */}
+      <NavLink to={`/notes-library/${isbn}`}>
+        <div
+          key={index}
+          isbn={isbn}
+          className="flex items-center p-2 rounded-lg text-base font-normal text-gray-900 dark:text-white bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 hover:cursor-pointer"
+        >
+          <span className="ml-3">{title}</span>
+        </div>
+      </NavLink>
     </li>
   );
 }
