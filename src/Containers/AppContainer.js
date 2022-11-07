77fc44152b5214a34f74ff0c78c2aca8626139ab
Darric Heng <darricheng@gmail.com>
2022-11-07 14:32:18 +0800

    Adjust number of results shown to just top 20

--- src/Containers/AppContainer.js
+++ src/Containers/AppContainer.js
@@ -17,7 +17,8 @@ export default function AppContainer() {
     try {
       const response = await fetch(query);
       const resObj = await response.json();
-      setSearchResults(resObj.docs);
+      // Just show the top 20 results
+      setSearchResults(resObj.docs.slice(0, 20));
     } catch {
       console.log("ERROR making api call");
     }
