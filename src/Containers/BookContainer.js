20281552c3e249c077c2f9415b18cfedd0f22ec3
Darric Heng <darricheng@gmail.com>
2022-11-08 16:54:28 +0800

    Shift save btn to a more universal location

--- src/Containers/BookContainer.js
+++ src/Containers/BookContainer.js
@@ -2,14 +2,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { addNote, deleteLibBook } from "../Redux/librarySlice";
 import Note from "../Components/Note";
-import { saveState } from "../localStorage";
-import { useState } from "react";
-import AlertLayout from "../Layouts/AlertLayout";
 
 export default function BookContainer() {
   const navigate = useNavigate();
-
-  const [isSaving, setIsSaving] = useState(false);
 
   // Get the isbn from the url
   const params = useParams();
@@ -20,12 +15,6 @@ export default function BookContainer() {
   const book = lib[isbn];
   const notes = book.notes;
   console.log(notes);
-
-  const save = () => {
-    saveState(lib);
-    setIsSaving(true);
-    setTimeout(() => setIsSaving(false), 2000);
-  };
 
   const handleDeleteBook = () => {
     const payload = { isbn: isbn };
@@ -53,22 +42,11 @@ export default function BookContainer() {
       </button>
       <div className="sticky top-48 left-0 -translate-y-24 z-50">
         <button
-          className="absolute top-0 right-0 px-6 py-1 bg-blue-500 hover:bg-blue-400 rounded-full text-white text-xl font-semibold"
-          onClick={() => save()}
-        >
-          Save Notes
-        </button>
-        <button
           className="absolute top-12 right-0 px-6 py-1 border-2 border-blue-500 bg-gray-200 hover:bg-gray-300 rounded-full text-gray-700 text-xl font-semibold"
           onClick={() => dispatch(addNote({ isbn: isbn }))}
         >
           Add New Note
         </button>
-        {isSaving && (
-          <AlertLayout coords={"top-1 right-40"}>
-            <p>Notes Saved</p>
-          </AlertLayout>
-        )}
       </div>
       <div id="note-sections">{noteSections}</div>
     </div>
--- src/Layouts/SidebarLayout.js
+++ src/Layouts/SidebarLayout.js
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { saveState } from "../localStorage";
+import { useState } from "react";
 import SidebarCard from "../Components/SidebarCard";
+import AlertLayout from "../Layouts/AlertLayout";
 
 export default function SidebarLayout() {
   const lib = useSelector((state) => state.library.value);
@@ -14,6 +17,14 @@ export default function SidebarLayout() {
       />
     );
   });
+
+  // Saving the collection to localStorage
+  const [isSaving, setIsSaving] = useState(false);
+  const save = () => {
+    saveState(lib);
+    setIsSaving(true);
+    setTimeout(() => setIsSaving(false), 2000);
+  };
 
   return (
     // https://flowbite.com/docs/components/sidebar/#default-sidebar
@@ -34,6 +45,19 @@ export default function SidebarLayout() {
         </ul>
       </aside>
       <div className="another-container ml-64 p-12">
+        <div className="sticky top-48 left-0 -translate-y-24 z-50">
+          <button
+            className="absolute top-0 right-0 px-6 py-1 bg-blue-500 hover:bg-blue-400 rounded-full text-white text-xl font-semibold"
+            onClick={() => save()}
+          >
+            Save Collection
+          </button>
+          {isSaving && (
+            <AlertLayout coords={"top-1 right-48"}>
+              <p>Notes Saved</p>
+            </AlertLayout>
+          )}
+        </div>
         {/* <Outlet /> is where props.children is rendered */}
         <Outlet />
       </div>
